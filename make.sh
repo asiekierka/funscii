c8b46946e92605b48232e57315a365d03ee17009
Adrian Siekierka <kontakt@asie.pl>
2023-06-03 20:45:16 +0200

    OpenComputers 1.8.2 font build

--- make.sh
+++ make.sh
@@ -12,4 +12,4 @@ cd ..
 
 BPATH=builder/build/libs/builder-0.1.0.jar
 java -cp $BPATH pl.asie.fontbuilder.Main font-width8.txt:unifont 16 hex out/funscii-8x16-unifont.hex
-java -cp $BPATH pl.asie.fontbuilder.Main font-width8.txt:unifont 16 bmp out/funscii-8x16-unifont-coverage.bmp 256 512
+java -cp $BPATH pl.asie.fontbuilder.Main font-width8.txt:unifont 16 bmp out/funscii-8x16-unifont-coverage.bmp 1024 1024
